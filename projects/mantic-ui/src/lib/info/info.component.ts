000b4880362c696b017efd55e94d6b02d074ec67
KY <kai@ky-programming.de>
2020-02-16 07:32:38 +0100

    error and info added

---
+++ projects/mantic-ui/src/lib/info/info.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, Input } from '@angular/core';
+import { ElementBase } from '../base/element-base';
+
+@Component({
+  selector: 'm-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.scss']
+})
+export class InfoComponent extends ElementBase {
+
+  @Input()
+  public ignored: boolean;
+
+  constructor(
+    readonly elementRef: ElementRef<HTMLElement>
+  ) {
+    super(elementRef);
+    this.classList
+      .registerBoolean('ignored')
+      .registerFixed('info', Number.MAX_VALUE - 2)
+      .registerFixed('message', Number.MAX_VALUE - 1);
+  }
+
+}
