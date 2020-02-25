ef80244c0de048c80b275b6d0ad0b94c61a1beff
KY <kai@ky-programming.de>
2020-02-25 15:42:46 +0100

    tab selectedIndex added

--- projects/mantic-ui/src/lib/tab-group/tab-group.component.ts
+++ projects/mantic-ui/src/lib/tab-group/tab-group.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ContentChildren, ElementRef, HostBinding, Input, OnInit, QueryList } from '@angular/core';
+import { AfterViewInit, Component, ContentChildren, ElementRef, EventEmitter, HostBinding, Input, OnInit, Output, QueryList } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { take } from 'rxjs/operators';
 import { MenuComponent } from '../menu/menu.component';
@@ -10,6 +10,7 @@ import { TabComponent } from '../tab/tab.component';
   styleUrls: ['./tab-group.component.scss']
 })
 export class TabGroupComponent extends MenuComponent implements OnInit, AfterViewInit {
+  private selectedIndexField: number;
 
   @ContentChildren(TabComponent)
   public tabs: QueryList<TabComponent>;
@@ -26,6 +27,20 @@ export class TabGroupComponent extends MenuComponent implements OnInit, AfterVie
   @Input()
   public routeParameterName = 'tab';
 
+  @Input()
+  public set selectedIndex(value: number) {
+    this.selectedIndexField = value;
+    if (this.tabs) {
+      this.tabs.forEach((tab, index) => tab.active = index === value);
+    }
+  }
+  public get selectedIndex(): number {
+    return this.selectedIndexField;
+  }
+
+  @Output()
+  public readonly selectedIndexChange = new EventEmitter<number>();
+
   constructor(
     private readonly route: ActivatedRoute,
     elementRef: ElementRef<HTMLElement>
@@ -36,7 +51,9 @@ export class TabGroupComponent extends MenuComponent implements OnInit, AfterVie
 
   public ngAfterViewInit(): void {
     if (this.tabs && this.tabs.length > 0 && this.tabs.toArray().every(tab => !tab.active)) {
-      setTimeout(() => this.tabs.first.active = true);
+      setTimeout(() => {
+        this.tabs.forEach((tab, index) => tab.active = index === (this.selectedIndex || 0));
+      });
     }
     if (this.selectByRoute) {
       this.route.params.pipe(take(1)).subscribe(params => {
@@ -56,5 +73,7 @@ export class TabGroupComponent extends MenuComponent implements OnInit, AfterVie
   public activate(tab: TabComponent): void {
     this.tabs.forEach(t => t.active = false);
     tab.active = true;
+    this.selectedIndex = this.tabs.toArray().indexOf(tab);
+    this.selectedIndexChange.emit(this.selectedIndex);
   }
 }
--- src/app/semantic/tab/tab.component.ts
+++ src/app/semantic/tab/tab.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab.component.scss']
 })
 export class SemanticTabComponent {
+  public selectedTab = 1;
+
   public code1 = `<m-tab-group>
   <m-tab label="Tab"></m-tab>
 </m-tab-group>`;
@@ -12,5 +14,11 @@ export class SemanticTabComponent {
   public code2 = `<m-tab-group>
   <m-tab label="Bio"></m-tab>
   <m-tab label="Photos" active></m-tab>
+</m-tab-group>`;
+
+  public selectedIndexCode = `<m-tab-group [(selectedIndex)]="selectedTab">
+  <m-tab></m-tab>
+  <m-tab></m-tab>
+  <m-tab></m-tab>
 </m-tab-group>`;
 }
