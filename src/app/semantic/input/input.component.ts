64deeaf53536e1c44dfa30be1a02e8954658bbcc
KY <kai@ky-programming.de>
2020-06-13 06:28:35 +0200

    input examples added

--- src/app/semantic/input/input.component.ts
+++ src/app/semantic/input/input.component.ts
@@ -72,4 +72,37 @@ export class SemanticInputComponent {
   public standardCode = `<m-input>
   <input type="text" placeholder="Search..." maxlength="3">
 </m-input>`;
+  public readonly states1 = `<m-input type="text" placeholder="Search..." focused></m-input>`;
+  public readonly states2 = `<m-input type="text" placeholder="Search..." icon="search" iconPosition="left" loading></m-input>`;
+  public readonly states3 = `<m-input type="text" placeholder="Search..." icon="search" loading></m-input>`;
+  public readonly states4 = `<m-input type="text" placeholder="Search..." disabled></m-input>`;
+  public readonly states5 = `<m-input type="text" placeholder="Search..." icon="search" disabled></m-input>`;
+  public readonly states6 = `<m-input type="text" placeholder="Search..." hasError></m-input>`;
+
+  public readonly variations1 = `<m-input type="text" placeholder="Search..." icon="search"></m-input>`;
+  public readonly variations2 = `<m-input type="text" placeholder="Search..." icon="users" iconPosition="left"></m-input>`;
+  public readonly variations3 = `<m-input type="text" placeholder="Search..." icon="circular search"></m-input>`;
+  public readonly variations4 = `<m-input type="text" placeholder="Search..." icon="inverted circular search"></m-input>`;
+  public readonly variations5 = `<m-input type="text" placeholder="mysite.com">
+  <m-label>http://</m-label>
+</m-input>`;
+  public readonly variations6 = `<m-input type="text" placeholder="Find domain">
+  <m-label-dropdown position="right" value=".com" [items]="['.com', '.net', '.org']"></m-label-dropdown>
+</m-input>`;
+  public readonly variations7 = `<m-input type="number" placeholder="Enter weight..">
+  <m-label position="right" class="basic">kg</m-label>
+</m-input>`;
+  public readonly variations8 = `<div class="ui right labeled input">
+  <label for="amount" class="ui label">$</label>
+  <input type="text" placeholder="Amount" id="amount">
+  <div class="ui basic label">.00</div>
+</div>`;
+  public readonly variations9 = `<m-input icon="tags" iconPosition="left" placeholder="Enter tags">
+  <m-label position="right" class="tag">Add Tag</m-label>
+</m-input>`;
+  public readonly variations10 = `<m-input type="text" class="corner" placeholder="Search...">
+  <m-label position="left" class="left corner">
+      <m-icon icon="asterisk"></m-icon>
+  </m-label>
+</m-input>`;
 }
