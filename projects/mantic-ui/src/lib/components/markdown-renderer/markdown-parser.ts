4bd0c7ac63b15a35d6592dd26915603543b6fcc5
KY <kai@ky-programming.de>
2023-05-10 05:41:50 +0200

    - some markdown parse errors fixed

---
+++ projects/mantic-ui/src/lib/components/markdown-renderer/markdown-parser.ts
@@ -0,0 +1,251 @@
+import { Injectable } from '@angular/core';
+import { MarkdownBlockParser } from './models/markdown-block-parser';
+import { MarkdownInlineParser } from './models/markdown-inline-parser';
+import { MarkdownHeader1, markdownHeader1Type, MarkdownHeader2, markdownHeader2Type, MarkdownHeader3, markdownHeader3Type, MarkdownHeader4, markdownHeader4Type, MarkdownHeader5, markdownHeader5Type, MarkdownHeader6, markdownHeader6Type } from './models/markdown-header';
+import { MarkdownCodeBlock, markdownCodeBlockType } from './models/markdown-code-block';
+import { MarkdownSeparator, markdownSeparatorType } from './models/markdown-separator';
+import { MarkdownElement } from './models/markdown-element';
+import { cast } from '../../helpers/cast';
+import { MarkdownCode, markdownCodeType } from './models/markdown-code';
+import { MarkdownImage, markdownImageType } from './models/markdown-image';
+import { MarkdownLink, markdownLinkType } from './models/markdown-link';
+import { MarkdownItalic, markdownItalicType } from './models/markdown-italic';
+import { MarkdownBold, markdownBoldType } from './models/markdown-bold';
+import { MarkdownStrikethrough, markdownStrikethroughType } from './models/markdown-strikethrough';
+import { MarkdownParagraph } from './models/markdown-paragraph';
+import { MarkdownText, markdownTextType } from './models/markdown-text';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MarkdownParser {
+    private readonly blockParsers: MarkdownBlockParser[] = [];
+    private readonly inlineParsers: MarkdownInlineParser[] = [];
+
+    public constructor() {
+        this.registerBlockParser({
+            regex: /^\s*#(?<inline>[^#].*)$/,
+            factory: (result: RegExpExecArray): MarkdownHeader1 => ({
+                type: markdownHeader1Type,
+                elements: this.parseInline(result.groups?.['inline']?.trim()),
+                isBlock: true
+            })
+        });
+        this.registerBlockParser({
+            regex: /^\s*#{2}(?<inline>[^#].*)$/,
+            factory: (result: RegExpExecArray): MarkdownHeader2 => ({
+                type: markdownHeader2Type,
+                elements: this.parseInline(result.groups?.['inline']?.trim()),
+                isBlock: true
+            })
+        });
+        this.registerBlockParser({
+            regex: /^\s*#{3}(?<inline>[^#].*)$/,
+            factory: (result: RegExpExecArray): MarkdownHeader3 => ({
+                type: markdownHeader3Type,
+                elements: this.parseInline(result.groups?.['inline']?.trim()),
+                isBlock: true
+            })
+        });
+        this.registerBlockParser({
+            regex: /^\s*#{4}(?<inline>[^#].*)$/,
+            factory: (result: RegExpExecArray): MarkdownHeader4 => ({
+                type: markdownHeader4Type,
+                elements: this.parseInline(result.groups?.['inline']?.trim()),
+                isBlock: true
+            })
+        });
+        this.registerBlockParser({
+            regex: /^\s*#{5}(?<inline>[^#].*)$/,
+            factory: (result: RegExpExecArray): MarkdownHeader5 => ({
+                type: markdownHeader5Type,
+                elements: this.parseInline(result.groups?.['inline']?.trim()),
+                isBlock: true
+            })
+        });
+        this.registerBlockParser({
+            regex: /^\s*#{6}(?<inline>[^#].*)$/,
+            factory: (result: RegExpExecArray): MarkdownHeader6 => ({
+                type: markdownHeader6Type,
+                elements: this.parseInline(result.groups?.['inline']?.trim()),
+                isBlock: true
+            })
+        });
+        this.registerBlockParser({
+            regex: /^\s*`{3}(?<language>.*?)\r?\n(?<code>.*?)`{3}$/g,
+            factory: (result: RegExpExecArray): MarkdownCodeBlock => ({
+                type: markdownCodeBlockType,
+                language: result.groups?.['language'],
+                code: result.groups?.['code'],
+                isBlock: true
+            })
+        });
+        this.registerBlockParser({
+            regex: /^\s*-{3}\s*$/,
+            factory: (): MarkdownSeparator => ({
+                type: markdownSeparatorType,
+                isBlock: true
+            })
+        });
+
+        this.registerInlineParser({
+            regex: /`(?<code>[^`].*?)`(?<post>.*)/,
+            factory: (result: RegExpExecArray): MarkdownElement[] => [
+                ...this.parseInline(result.input.substring(0, result.index)),
+                cast<MarkdownCode>({
+                    type: markdownCodeType,
+                    code: result.groups?.['code']
+                }),
+                ...this.parseInline(result.groups?.['post'])
+            ]
+        });
+        this.registerInlineParser({
+            regex: /!\[(?<alt>.*)]\((?<url>.*)\)(?<post>.*)/,
+            factory: (result: RegExpExecArray): MarkdownElement[] => [
+                ...this.parseInline(result.input.substring(0, result.index)),
+                cast<MarkdownImage>({
+                    type: markdownImageType,
+                    url: result.groups?.['url'] ?? '',
+                    alt: result.groups?.['alt'] ?? ''
+                }),
+                ...this.parseInline(result.groups?.['post'])
+            ]
+        });
+        this.registerInlineParser({
+            regex: /\[(?<inline>.*)]\((?<url>.*)\)(?<post>.*)/,
+            factory: (result: RegExpExecArray): MarkdownElement[] => [
+                ...this.parseInline(result.input.substring(0, result.index)),
+                cast<MarkdownLink>({
+                    type: markdownLinkType,
+                    url: result.groups?.['url'] ?? '',
+                    elements: this.parseInline(result.groups?.['inline'])
+                }),
+                ...this.parseInline(result.groups?.['post'])
+            ]
+        });
+        this.registerInlineParser({
+            regex: /\*(?<inline>.*?)\*(?<post>.*)/,
+            factory: (result: RegExpExecArray): MarkdownElement[] => [
+                ...this.parseInline(result.input.substring(0, result.index)),
+                cast<MarkdownItalic>({
+                    type: markdownItalicType,
+                    elements: this.parseInline(result.groups?.['inline'])
+                }),
+                ...this.parseInline(result.groups?.['post'])
+            ]
+        });
+        this.registerInlineParser({
+            regex: /_(?<inline>.*?)_(?<post>.*)/,
+            factory: (result: RegExpExecArray): MarkdownElement[] => [
+                ...this.parseInline(result.input.substring(0, result.index)),
+                cast<MarkdownItalic>({
+                    type: markdownItalicType,
+                    elements: this.parseInline(result.groups?.['inline'])
+                }),
+                ...this.parseInline(result.groups?.['post'])
+            ]
+        });
+        this.registerInlineParser({
+            regex: /\*\*(?<inline>.*?)\*\*(?<post>.*)/,
+            factory: (result: RegExpExecArray): MarkdownElement[] => [
+                ...this.parseInline(result.input.substring(0, result.index)),
+                cast<MarkdownBold>({
+                    type: markdownBoldType,
+                    elements: this.parseInline(result.groups?.['inline'])
+                }),
+                ...this.parseInline(result.groups?.['post'])
+            ]
+        });
+        this.registerInlineParser({
+            regex: /__(?<inline>.*?)__(?<post>.*)/,
+            factory: (result: RegExpExecArray): MarkdownElement[] => [
+                ...this.parseInline(result.input.substring(0, result.index)),
+                cast<MarkdownBold>({
+                    type: markdownBoldType,
+                    elements: this.parseInline(result.groups?.['inline'])
+                }),
+                ...this.parseInline(result.groups?.['post'])
+            ]
+        });
+        this.registerInlineParser({
+            regex: /~~(?<inline>.*?)~~(?<post>.*)/,
+            factory: (result: RegExpExecArray): MarkdownElement[] => [
+                ...this.parseInline(result.input.substring(0, result.index)),
+                cast<MarkdownStrikethrough>({
+                    type: markdownStrikethroughType,
+                    elements: this.parseInline(result.groups?.['inline'])
+                }),
+                ...this.parseInline(result.groups?.['post'])
+            ]
+        });
+
+    }
+
+    public registerBlockParser(parser: MarkdownBlockParser): void {
+        this.blockParsers.push(parser);
+    }
+
+    public registerInlineParser(parser: MarkdownInlineParser): void {
+        this.inlineParsers.push(parser);
+    }
+
+    public parse(value: string): MarkdownElement[] {
+        const elements: MarkdownElement[] = [];
+        if (value.includes('\n\n')) {
+            const paragraphs = value.split('\n\n');
+            for (const paragraph of paragraphs) {
+                elements.push(cast<MarkdownParagraph>({
+                    type: 'paragraph',
+                    elements: this.parseBlock(paragraph),
+                    isBlock: true
+                }));
+            }
+        } else {
+            this.parseBlock(value);
+        }
+        return elements;
+    }
+
+    private parseBlock(text: string): MarkdownElement[] {
+        const lines = text.split('\n');
+        const elements: MarkdownElement[] = [];
+        for (const line of lines) {
+            let parsed = false;
+            for (const parser of this.blockParsers) {
+                const regexResult = parser.regex.exec(line);
+                if (regexResult) {
+                    elements.push(parser.factory(regexResult));
+                    parsed = true;
+                    break;
+                }
+            }
+            if (!parsed) {
+                elements.push(...this.parseInline(line));
+            }
+            elements[elements.length - 1].breakLine = true;
+        }
+        return elements;
+    }
+
+    private parseInline(text: string | undefined): MarkdownElement[] {
+        if (!text) {
+            return [];
+        }
+        const elements: MarkdownElement[] = [];
+        let firstResult: RegExpExecArray | undefined;
+        let firstParser: MarkdownInlineParser | undefined;
+        for (const parser of this.inlineParsers) {
+            const regexResult = parser.regex.exec(text);
+            if (regexResult && (!firstResult || regexResult.index <= firstResult.index)) {
+                firstResult = regexResult;
+                firstParser = parser;
+            }
+        }
+        if (firstResult && firstParser) {
+            elements.push(...firstParser.factory(firstResult));
+        } else {
+            elements.push(cast<MarkdownText>({ type: markdownTextType, text }));
+        }
+        return elements;
+    }
+}
--- projects/mantic-ui/src/lib/components/markdown-renderer/markdown-renderer.component.ts
+++ projects/mantic-ui/src/lib/components/markdown-renderer/markdown-renderer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { isMarkdownHeader1, isMarkdownHeader2, isMarkdownHeader3, isMarkdownHeader4, isMarkdownHeader5, isMarkdownHeader6, MarkdownHeader1, markdownHeader1Type, MarkdownHeader2, markdownHeader2Type, MarkdownHeader3, markdownHeader3Type, MarkdownHeader4, markdownHeader4Type, MarkdownHeader5, markdownHeader5Type, MarkdownHeader6, markdownHeader6Type } from './models/markdown-header';
 import { isMarkdownLink, MarkdownLink, markdownLinkType } from './models/markdown-link';
@@ -7,14 +7,12 @@ import { MarkdownElement } from './models/markdown-element';
 import { isMarkdownText, MarkdownText, markdownTextType } from './models/markdown-text';
 import { isMarkdownItalic, MarkdownItalic, markdownItalicType } from './models/markdown-italic';
 import { isMarkdownBold, MarkdownBold, markdownBoldType } from './models/markdown-bold';
-import { MarkdownBlockParser } from './models/markdown-block-parser';
-import { MarkdownInlineParser } from './models/markdown-inline-parser';
-import { cast } from '../../helpers/cast';
 import { isMarkdownImage, MarkdownImage, markdownImageType } from './models/markdown-image';
 import { isMarkdownStrikethrough, MarkdownStrikethrough, markdownStrikethroughType } from './models/markdown-strikethrough';
 import { isMarkdownCodeBlock, MarkdownCodeBlock, markdownCodeBlockType } from './models/markdown-code-block';
 import { isMarkdownCode, MarkdownCode, markdownCodeType } from './models/markdown-code';
-import { MarkdownSeparator, markdownSeparatorType } from './models/markdown-separator';
+import { markdownSeparatorType } from './models/markdown-separator';
+import { MarkdownParser } from './markdown-parser';
 
 @Component({
     selector: 'm-markdown-renderer',
@@ -24,153 +22,9 @@ import { MarkdownSeparator, markdownSeparatorType } from './models/markdown-sepa
     styleUrls: ['./markdown-renderer.component.scss']
 })
 export class MarkdownRendererComponent {
+    private readonly parser = inject(MarkdownParser);
     private rawValue = '';
-    private readonly blockParsers: MarkdownBlockParser[] = [
-        {
-            regex: /^\s*#(?<inline>[^#].*)$/,
-            factory: (result: RegExpExecArray): MarkdownHeader1 => ({
-                type: markdownHeader1Type,
-                elements: this.parseInline(result.groups?.['inline']?.trim()),
-                isBlock: true
-            })
-        }, {
-            regex: /^\s*#{2}(?<inline>[^#].*)$/,
-            factory: (result: RegExpExecArray): MarkdownHeader2 => ({
-                type: markdownHeader2Type,
-                elements: this.parseInline(result.groups?.['inline']?.trim()),
-                isBlock: true
-            })
-        }, {
-            regex: /^\s*#{3}(?<inline>[^#].*)$/,
-            factory: (result: RegExpExecArray): MarkdownHeader3 => ({
-                type: markdownHeader3Type,
-                elements: this.parseInline(result.groups?.['inline']?.trim()),
-                isBlock: true
-            })
-        }, {
-            regex: /^\s*#{4}(?<inline>[^#].*)$/,
-            factory: (result: RegExpExecArray): MarkdownHeader4 => ({
-                type: markdownHeader4Type,
-                elements: this.parseInline(result.groups?.['inline']?.trim()),
-                isBlock: true
-            })
-        }, {
-            regex: /^\s*#{5}(?<inline>[^#].*)$/,
-            factory: (result: RegExpExecArray): MarkdownHeader5 => ({
-                type: markdownHeader5Type,
-                elements: this.parseInline(result.groups?.['inline']?.trim()),
-                isBlock: true
-            })
-        }, {
-            regex: /^\s*#{6}(?<inline>[^#].*)$/,
-            factory: (result: RegExpExecArray): MarkdownHeader6 => ({
-                type: markdownHeader6Type,
-                elements: this.parseInline(result.groups?.['inline']?.trim()),
-                isBlock: true
-            })
-        }, {
-            regex: /^\s*`{3}(?<language>.*?)\r?\n(?<code>.*?)`{3}$/g,
-            factory: (result: RegExpExecArray): MarkdownCodeBlock => ({
-                type: markdownCodeBlockType,
-                language: result.groups?.['language'],
-                code: result.groups?.['code'],
-                isBlock: true
-            })
-        }, {
-            regex: /^\s*-{3}\s*$/,
-            factory: (): MarkdownSeparator => ({
-                type: markdownSeparatorType,
-                isBlock: true
-            })
-        }
-    ];
-
-    private readonly inlineParsers: MarkdownInlineParser[] = [
-        {
-            regex: /`(?<code>[^`].*?)`(?<post>.*)/,
-            factory: (result: RegExpExecArray): MarkdownElement[] => [
-                ...this.parseInline(result.input.substring(0, result.index)),
-                cast<MarkdownCode>({
-                    type: markdownCodeType,
-                    code: result.groups?.['code']
-                }),
-                ...this.parseInline(result.groups?.['post'])
-            ]
-        }, {
-            regex: /!\[(?<alt>.*)]\((?<url>.*)\)(?<post>.*)/,
-            factory: (result: RegExpExecArray): MarkdownElement[] => [
-                ...this.parseInline(result.input.substring(0, result.index)),
-                cast<MarkdownImage>({
-                    type: markdownImageType,
-                    url: result.groups?.['url'] ?? '',
-                    alt: result.groups?.['alt'] ?? ''
-                }),
-                ...this.parseInline(result.groups?.['post'])
-            ]
-        }, {
-            regex: /\[(?<inline>.*)]\((?<url>.*)\)(?<post>.*)/,
-            factory: (result: RegExpExecArray): MarkdownElement[] => [
-                ...this.parseInline(result.input.substring(0, result.index)),
-                cast<MarkdownLink>({
-                    type: markdownLinkType,
-                    url: result.groups?.['url'] ?? '',
-                    elements: this.parseInline(result.groups?.['inline'])
-                }),
-                ...this.parseInline(result.groups?.['post'])
-            ]
-        }, {
-            regex: /\*(?<inline>.*?)\*(?<post>.*)/,
-            factory: (result: RegExpExecArray): MarkdownElement[] => [
-                ...this.parseInline(result.input.substring(0, result.index)),
-                cast<MarkdownItalic>({
-                    type: markdownItalicType,
-                    elements: this.parseInline(result.groups?.['inline'])
-                }),
-                ...this.parseInline(result.groups?.['post'])
-            ]
-        }, {
-            regex: /_(?<inline>.*?)_(?<post>.*)/,
-            factory: (result: RegExpExecArray): MarkdownElement[] => [
-                ...this.parseInline(result.input.substring(0, result.index)),
-                cast<MarkdownItalic>({
-                    type: markdownItalicType,
-                    elements: this.parseInline(result.groups?.['inline'])
-                }),
-                ...this.parseInline(result.groups?.['post'])
-            ]
-        }, {
-            regex: /\*\*(?<inline>.*?)\*\*(?<post>.*)/,
-            factory: (result: RegExpExecArray): MarkdownElement[] => [
-                ...this.parseInline(result.input.substring(0, result.index)),
-                cast<MarkdownBold>({
-                    type: markdownBoldType,
-                    elements: this.parseInline(result.groups?.['inline'])
-                }),
-                ...this.parseInline(result.groups?.['post'])
-            ]
-        }, {
-            regex: /__(?<inline>.*?)__(?<post>.*)/,
-            factory: (result: RegExpExecArray): MarkdownElement[] => [
-                ...this.parseInline(result.input.substring(0, result.index)),
-                cast<MarkdownBold>({
-                    type: markdownBoldType,
-                    elements: this.parseInline(result.groups?.['inline'])
-                }),
-                ...this.parseInline(result.groups?.['post'])
-            ]
-        }, {
-            regex: /~~(?<inline>.*?)~~(?<post>.*)/,
-            factory: (result: RegExpExecArray): MarkdownElement[] => [
-                ...this.parseInline(result.input.substring(0, result.index)),
-                cast<MarkdownStrikethrough>({
-                    type: markdownStrikethroughType,
-                    elements: this.parseInline(result.groups?.['inline'])
-                }),
-                ...this.parseInline(result.groups?.['post'])
-            ]
-        }
-    ];
-    protected readonly elements: MarkdownElement[] = [];
+    protected elements: MarkdownElement[] = [];
     protected readonly markdownParagraphType = markdownParagraphType;
     protected readonly markdownHeader1Type = markdownHeader1Type;
     protected readonly markdownHeader2Type = markdownHeader2Type;
@@ -199,65 +53,7 @@ export class MarkdownRendererComponent {
         if (!value) {
             return;
         }
-        this.parse();
-    }
-
-    private parse(): void {
-        if (this.value.includes('\n\n')) {
-            const paragraphs = this.value.split('\n\n');
-            for (const paragraph of paragraphs) {
-                this.elements.push(cast<MarkdownParagraph>({
-                    type: 'paragraph',
-                    elements: this.parseBlock(paragraph),
-                    isBlock: true
-                }));
-            }
-        } else {
-            this.parseBlock(this.value);
-        }
-    }
-
-    private parseBlock(text: string): MarkdownElement[] {
-        const lines = text.split('\n');
-        const elements: MarkdownElement[] = [];
-        for (const line of lines) {
-            let parsed = false;
-            for (const parser of this.blockParsers) {
-                const regexResult = parser.regex.exec(line);
-                if (regexResult) {
-                    elements.push(parser.factory(regexResult));
-                    parsed = true;
-                    break;
-                }
-            }
-            if (!parsed) {
-                elements.push(...this.parseInline(line));
-            }
-            elements[elements.length - 1].breakLine = true;
-        }
-        return elements;
-    }
-
-    private parseInline(text: string | undefined): MarkdownElement[] {
-        if (!text) {
-            return [];
-        }
-        const elements: MarkdownElement[] = [];
-        let firstResult: RegExpExecArray | undefined;
-        let firstParser: MarkdownInlineParser | undefined;
-        for (const parser of this.inlineParsers) {
-            const regexResult = parser.regex.exec(text);
-            if (regexResult && (!firstResult || regexResult.index <= firstResult.index)) {
-                firstResult = regexResult;
-                firstParser = parser;
-            }
-        }
-        if (firstResult && firstParser) {
-            elements.push(...firstParser.factory(firstResult));
-        } else {
-            elements.push(cast<MarkdownText>({ type: markdownTextType, text }));
-        }
-        return elements;
+        this.elements = this.parser.parse(value);
     }
 
     protected $elements(elements: []): MarkdownElement[] {
