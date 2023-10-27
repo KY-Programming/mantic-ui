cd82006c8e55f1e4349ac37baa3792d936e7eba5
KY <kai@ky-programming.de>
2023-10-27 13:56:43 +0200

    Helpers feat: date-helper addition function

--- projects/mantic-ui/src/lib/helpers/date-helper.ts
+++ projects/mantic-ui/src/lib/helpers/date-helper.ts
@@ -1,4 +1,6 @@
-﻿export abstract class DateHelper {
+﻿import { Timespan } from './timespan';
+
+export abstract class DateHelper {
     public static keepInRange(min: Date, value: Date | undefined, max?: Date | undefined): Date
     public static keepInRange(min: Date | undefined, value: Date, max?: Date | undefined): Date
     public static keepInRange(min: Date | undefined, value: Date | undefined, max?: Date): Date
@@ -16,7 +18,32 @@
         if (!left || !right) {
             return false;
         }
-        return left.getFullYear() === right.getFullYear() && left.getMonth() === right.getMonth() && left.getDate() === right.getDate();
+        return this.sameMonth(left, right) && left.getDate() === right.getDate();
+    }
+
+    public static sameWeek(left: Date | undefined, right: Date | undefined): boolean {
+        if (!left || !right) {
+            return false;
+        }
+        const leftDayOfYear = Math.floor((left.getTime() - new Date(left.getFullYear(), 0, 0).getTime()) / Timespan.millisecondsInDay);
+        const rightDayOfYear = Math.floor((right.getTime() - new Date(right.getFullYear(), 0, 0).getTime()) / Timespan.millisecondsInDay);
+        const leftWeekNumber = Math.ceil((leftDayOfYear + 1) / 7);
+        const rightWeekNumber = Math.ceil((rightDayOfYear + 1) / 7);
+        return leftWeekNumber === rightWeekNumber;
+    }
+
+    public static sameMonth(left: Date | undefined, right: Date | undefined): boolean {
+        if (!left || !right) {
+            return false;
+        }
+        return this.sameYear(left, right) && left.getMonth() === right.getMonth();
+    }
+
+    public static sameYear(left: Date | undefined, right: Date | undefined): boolean {
+        if (!left || !right) {
+            return false;
+        }
+        return left.getFullYear() === right.getFullYear();
     }
 
     public static today(): Date {
