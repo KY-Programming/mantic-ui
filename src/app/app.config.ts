59ee840cba28ac40cdd41c4a3d96625721aba944
KY <kai@ky-programming.de>
2023-11-18 10:56:01 +0100

    Upgraded to new project structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,8 @@
-import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { ButtonComponent, provideDynamicComponent, provideManticTitleStrategy } from '@mantic-ui/angular';
-import { provideHighlightJs } from '@mantic-ui/angular-doc';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        provideHttpClient(),
-        provideManticTitleStrategy({
-            postfix: ' - mantic UI',
-            fallback: 'mantic UI',
-            conditions: [
-                { condition: '/semantic', fallback: 'Semantic UI Angular - mantic UI', postfix: ' - Semantic UI Angular - mantic UI' },
-                { condition: '/fomantic', fallback: 'Fomantic UI Angular - mantic UI', postfix: ' - Fomantic UI Angular - mantic UI' }
-            ]
-        }),
-        provideHighlightJs(),
-        provideDynamicComponent(ButtonComponent)
-    ]
+  providers: [provideRouter(routes)]
 };
