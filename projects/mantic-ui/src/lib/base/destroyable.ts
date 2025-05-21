2c0d3cfd7ac7eb6c07f3aeb2b35c60dee059f532
KY <kai@ky-programming.de>
2025-05-21 14:52:04 +0200

    feat: make destroyable methods public

--- projects/mantic-ui/src/lib/base/destroyable.ts
+++ projects/mantic-ui/src/lib/base/destroyable.ts
@@ -5,14 +5,18 @@ import { takeUntil } from 'rxjs/operators';
 @Injectable()
 export abstract class Destroyable implements OnDestroy {
     private readonly destroySubject = new Subject<void>();
-    protected readonly destroy = this.destroySubject.asObservable();
+
+    /**
+     * An observable that emits when the component is destroyed.
+     */
+    public readonly destroy = this.destroySubject.asObservable();
 
     /**
      * Pipe this method to a subject to take values until the component is destroyed.
      * Example: `observable.pipe(this.takeUntilDestroy()).subscribe(...);`
      * Equals to `observable.pipe(takeUntil(this.destroy)).subscribe(...);`.
      */
-    protected takeUntilDestroy<T>(): MonoTypeOperatorFunction<T> {
+    public takeUntilDestroy<T>(): MonoTypeOperatorFunction<T> {
         return takeUntil(this.destroy);
     }
 
